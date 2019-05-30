bc84397c6b4b25f286a88424787b21539ddce0c3
Illkwon Ha <haik0925@gmail.com>
2019-05-31 01:06:27 +0900

    dont install packages already instaleld

--- scoop/install_packages.ps1
+++ scoop/install_packages.ps1
@@ -1,1 +1,2 @@
-cat package_list.txt | % {scoop install $_}
+$installed_packages = scoop export | cut -d' ' -f1
+cat package_list.txt | % { if ($installed_packages -notcontains $_) { scoop install $_ } else { echo "$_ is already installed" } }
