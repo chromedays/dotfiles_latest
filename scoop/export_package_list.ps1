8372c94846d802d8661eb48430fbb31641e59337
Illkwon Ha <haik0925@gmail.com>
2019-05-31 01:07:05 +0900

    commented out code exporting bucket list. gonna manually add them

--- scoop/export_package_list.ps1
+++ scoop/export_package_list.ps1
@@ -1,2 +1,2 @@
 scoop export | cut -d' ' -f1 > package_list.txt
-scoop export | cut -d' ' -f3 | tr -d '[]' | sort | uniq > bucket_list.txt
+# scoop export | cut -d' ' -f3 | tr -d '[]' | sort | uniq > bucket_list.txt
