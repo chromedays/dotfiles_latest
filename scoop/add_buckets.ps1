5cb92037b19bf8e4361e2c0c4e799321aaef6bf9
Illkwon Ha <haik0925@gmail.com>
2019-05-31 01:46:35 +0900

    modified add bucket script

--- scoop/add_buckets.ps1
+++ scoop/add_buckets.ps1
@@ -1,1 +1,5 @@
-cat bucket_list.txt | % {scoop bucket add $_}
+$added_buckets = scoop bucket list
+cat bucket_list.txt | % {
+    $bucket_name, $bucket_url = $_.split();
+    if ($added_buckets -notcontains $bucket_name) { scoop bucket add $bucket_name $bucket_url } else { echo "$bucket_name is already added" }
+}
